3e5e478d27a5975f3bcd3322163514ff008db99c
Henrique Granella <ooeike@hotmail.com>
2025-10-26 16:38:10 -0300

    Commit atrasado - alteração de pastas e criação do footer

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/layout/Header"
+import { Header } from "./components/layout/footer"
 
 function App() {
   return (
     <>
      <Header/>
+     <main></main>
+     <footer/>
     </>
   )
 }
